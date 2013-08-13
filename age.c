527f59b7d591ab2a70e1563112d01ddce284e9e5
Rudy Matela <rudy@matela.com.br>
2013-08-13 10:51:58 -0300

    Now effectively calculating file age.

--- age.c
+++ age.c
@@ -27,6 +27,7 @@
 
 
 static int stat_time(const char *path, struct timespec *buf, char type);
+static int stat_age(const char *path, struct timespec *buf, char type);
 
 
 int main(int argc, char **argv)
@@ -35,7 +36,7 @@ int main(int argc, char **argv)
 	static int help;
 	static int version;
 	
-	struct timespec timebuf;
+	struct timespec file_age;
 
 	struct soption opttable[] = {
 		{ 'h', "help",    0, capture_presence,    &help },
@@ -66,15 +67,33 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-	if (stat_time(argv[1],&timebuf,'m')!=0) {
+	if (stat_age(argv[1],&file_age,'m')!=0) {
 		char *progname = basename(argv[0]);
 		fprintf(stderr,"%s: error, unable to retrieve `%s' attributes\n", progname, argv[1]);
 		return 1;
 	}
-	
-	printf("%li.%09li\n", timebuf.tv_sec, timebuf.tv_nsec);
+
+	printf("%li.%09li\n", file_age.tv_sec, file_age.tv_nsec);
 
 	return 0;
+}
+
+
+static int stat_age(const char *path, struct timespec *buf, char type)
+{
+	struct timespec file_time;
+	struct timespec curr_time;
+	int r;
+	r = stat_time(path, &file_time, type) || clock_gettime(CLOCK_REALTIME, &curr_time);
+	if (r)
+		return r;
+	buf->tv_sec  = curr_time.tv_sec  - file_time.tv_sec;
+	buf->tv_nsec = curr_time.tv_nsec - file_time.tv_nsec;
+	if (buf->tv_nsec < 0) {
+		buf->tv_nsec += 1000000000;
+		buf->tv_sec --;
+	}
+	return r;
 }
 
 
