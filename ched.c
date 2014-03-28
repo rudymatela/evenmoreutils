33778b83dca16f5587634dd4cd106edf639e9480
Rudy Matela <rudy@matela.com.br>
2014-03-28 18:57:53 +0000

    Removed TODO.

--- ched.c
+++ ched.c
@@ -15,10 +15,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * TODO: This program needs error treating to be fixed (not checking if
- * functions that should fail are failing).
  */
 #include "sgetopt.h"
 #include "version.h"
