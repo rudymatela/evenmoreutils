01bc3da2359b17af28f6b9d50a915e0e75d0bc4f
Rudy Matela <rudy@matela.com.br>
2014-05-12 19:13:43 +0100

    Corrected references in test.sh.

--- bin/test.sh
+++ bin/test.sh
@@ -52,16 +52,16 @@ assert_evl() {
 
 
 sgetopt_test_test() {
-	assert ./sgetopt_test <<REF
+	assert ./src/sgetopt_test <<REF
 integer = 0, string = (null), float = 0.0, double = 0.0, bool(long) = 0
 REF
 
-	assert ./sgetopt_test -no <<REF
+	assert ./src/sgetopt_test -no <<REF
 0
 integer = 0, string = (null), float = 0.0, double = 0.0, bool(long) = 0
 REF
 
-	assert ./sgetopt_test -no a b c <<REF
+	assert ./src/sgetopt_test -no a b c <<REF
 3
 a
 b
@@ -69,22 +69,22 @@ c
 integer = 0, string = (null), float = 0.0, double = 0.0, bool(long) = 0
 REF
 
-	assert ./sgetopt_test -no wohoo <<REF
+	assert ./src/sgetopt_test -no wohoo <<REF
 1
 wohoo
 integer = 0, string = (null), float = 0.0, double = 0.0, bool(long) = 0
 REF
 
-	assert ./sgetopt_test -i 10 <<REF
+	assert ./src/sgetopt_test -i 10 <<REF
 integer = 10, string = (null), float = 0.0, double = 0.0, bool(long) = 0
 REF
 
-	assert ./sgetopt_test -no --string abcd <<REF
+	assert ./src/sgetopt_test -no --string abcd <<REF
 0
 integer = 0, string = abcd, float = 0.0, double = 0.0, bool(long) = 0
 REF
 
-	assert ./sgetopt_test -no a b c -f 123 --double 3.14 <<REF
+	assert ./src/sgetopt_test -no a b c -f 123 --double 3.14 <<REF
 3
 a
 b
@@ -92,26 +92,26 @@ c
 integer = 0, string = (null), float = 123.0, double = 3.1, bool(long) = 0
 REF
 
-	assert ./sgetopt_test -no wohoo --long <<REF
+	assert ./src/sgetopt_test -no wohoo --long <<REF
 1
 wohoo
 integer = 0, string = (null), float = 0.0, double = 0.0, bool(long) = 1
 REF
 
-	assert ./sgetopt_test -i asdf <<REF
+	assert ./src/sgetopt_test -i asdf <<REF
 Error parsing one of the command line options
 REF
 
-	assert ./sgetopt_test -iasdf <<REF
+	assert ./src/sgetopt_test -iasdf <<REF
 Error parsing one of the command line options
 REF
 
-	assert ./sgetopt_test -i <<REF
+	assert ./src/sgetopt_test -i <<REF
 Missing argument to \`-i'
 Error parsing one of the command line options
 REF
 
-	assert ./sgetopt_test --integer <<REF
+	assert ./src/sgetopt_test --integer <<REF
 Missing argument to \`--integer'
 Error parsing one of the command line options
 REF
@@ -119,13 +119,13 @@ REF
 
 
 randpar_test() {
-	assert_evl ./randpar    1 2 3       \| wc <<<"      1       1       2"
-	assert_evl ./randpar -a 1 2 3       \| wc <<<"      3       3       6"
-	assert_evl ./randpar -a 1x2 3       \| wc <<<"      2       2       6"
-	assert_evl ./randpar -a0 1x 3       \| wc <<<"      0       1       5"
-	assert_evl ./randpar -n 10 abc de f \| wc -l <<<"10"
-	assert_evl ./randpar `seq 1 100`    \| wc -l <<<"1"
-	assert_evl ./randpar 1 2 3 -n0 4    \| wc -l <<<"0"
+	assert_evl ./src/randpar    1 2 3       \| wc <<<"      1       1       2"
+	assert_evl ./src/randpar -a 1 2 3       \| wc <<<"      3       3       6"
+	assert_evl ./src/randpar -a 1x2 3       \| wc <<<"      2       2       6"
+	assert_evl ./src/randpar -a0 1x 3       \| wc <<<"      0       1       5"
+	assert_evl ./src/randpar -n 10 abc de f \| wc -l <<<"10"
+	assert_evl ./src/randpar `seq 1 100`    \| wc -l <<<"1"
+	assert_evl ./src/randpar 1 2 3 -n0 4    \| wc -l <<<"0"
 }
 
 
@@ -137,17 +137,17 @@ Well, this is a text file
 with two lines.
 REF
 
-	assert fit -w 10 $testfile <<REF
+	assert ./src/fit -w 10 $testfile <<REF
 Well, this
 with two l
 REF
 
-	assert pad -w 30 $testfile <<REF
+	assert ./src/pad -w 30 $testfile <<REF
 Well, this is a text file     
 with two lines.               
 REF
 
-	assert fit -w 20 --pad $testfile <<REF
+	assert ./src/fit -w 20 --pad $testfile <<REF
 Well, this is a text
 with two lines.     
 REF
@@ -158,20 +158,20 @@ REF
 
 ched_test() {
 	wd=`pwd`
-	assert ched echo "blah" <<REF
+	assert ./src/ched echo "blah" <<REF
 blah
 REF
-	assert ched echo "blah" <<REF
+	assert ./src/ched echo "blah" <<REF
 blah
 REF
 	assert cat ~/.cache/ched/"`echo -ne "echo\\x00blah\\x00$wd" | stdin_md5`" <<REF
 blah
 REF
-	assert ched echo -e "blah\nbleh" <<REF
+	assert ./src/ched echo -e "blah\nbleh" <<REF
 blah
 bleh
 REF
-	assert ched echo -e "blah\nbleh" <<REF
+	assert ./src/ched echo -e "blah\nbleh" <<REF
 blah
 bleh
 REF
